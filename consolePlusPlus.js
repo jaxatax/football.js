e4c26702cf091af85215e842a5d04f3996b681b7
jaxatax <42788733+jaxatax@users.noreply.github.com>
2019-05-07 19:09:17 -0500

    Create consolePlusPlus.js

---
+++ consolePlusPlus.js
@@ -0,0 +1,13 @@
+// User Input in NodeJS by Timmy Chen, Jaxatax, & topherPedersen
+
+const readLine = require("readline-sync");
+
+function input(prompt) {
+    const userInput = readLine.question(prompt)
+    return(userInput)
+}
+
+
+module.exports = {
+    input: input
+};
